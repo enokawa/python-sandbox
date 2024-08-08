6c83fce303349d1bcffa47b4b53a49bfb801be9e
enokawa <enokawa00@gmail.com>
2024-08-09 00:47:03 +0900

    Add logging

--- lambda-web-adapter/src/main.py
+++ lambda-web-adapter/src/main.py
@@ -1,8 +1,15 @@
+import logging
+
 from fastapi import FastAPI
+
+
+logger = logging.getLogger("uvicorn")
+logger.setLevel("INFO")
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
+    logger.info("test")
     return {"Hello": "World"}
